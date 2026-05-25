d96d297c62d549053b2221a2edcb2ad59d633ebe
Valsym <fanski@yandex.ru>
2026-05-25 17:14:30 +0300

    Install Laravel Octane in sail/Docker
     - Add laravel/octane with Swoole
     - Configure Supervisor to run octane:start (workers=8)

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
